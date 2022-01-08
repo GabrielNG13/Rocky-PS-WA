bf6309a755c8867ef725e6edcc40cf23db071e64
GabrielNG13 <gabrielnunesmghirardelli@gmail.com>
2022-01-08 12:42:21 -0300

    Fix quantities

--- files/resolucao.js
+++ files/resolucao.js
@@ -2,7 +2,7 @@ function readJson(){
     return require('./broken-database.json')
 }
 
-function fixNames(data){
+function fixName(data){
     data.forEach(obj => {
         var name = obj.name                 //Copia o valor de name
         name = name.replace(/æ/g,"a")       //Substitui "æ" por "a"
@@ -13,15 +13,26 @@ function fixNames(data){
     });
 }
 
-function fixPrices(data){
+function fixPrice(data){
     data.forEach(obj => {
-        var price = parseFloat(obj.price)   //Copia o valor de price e o converte para float
-        obj.price = price                   //Registra o valor corrigido
+        if(typeof obj.price == "string"){
+            var price = parseFloat(obj.price)   //Copia o valor de price e o converte para float
+            obj.price = price                   //Registra o valor corrigido
+        }
+    });
+}
+
+function fixQuantity(data){
+    data.forEach(obj => {
+        if(obj.quantity == undefined){
+            obj.quantity = 0
+        }
     });
 }
 
 const data = readJson();                    //Array com todos os objetos obtidos da leitura do JSON
-fixNames(data);                             //Chamada da função que corrige os nomes
-fixPrices(data);                            //Chamada da função que corrige os preços
+fixName(data);                              //Chamada da função que corrige os nomes
+fixPrice(data);                             //Chamada da função que corrige os preços
+fixQuantity(data);                          //Chamada da função que corrige as quantidades
 
 console.log(data)
