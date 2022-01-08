66c06d189070b5d5c2aafb51c3762a72fe3145db
GabrielNG13 <gabrielnunesmghirardelli@gmail.com>
2022-01-08 13:40:09 -0300

    Export solution

--- files/resolucao.js
+++ files/resolucao.js
@@ -1,10 +1,15 @@
 function readJson(){
-    return require('./broken-database.json')
+    try {
+        return require('./broken-database.json')
+    } catch (MODULE_NOT_FOUND){
+        console.log("File not found");
+        return null;
+    }
 }
 
 function fixName(data){
     data.forEach(obj => {
-        var name = obj.name                 //Copia o valor de name
+        let name = obj.name                 //Copia o valor de name
         name = name.replace(/æ/g,"a")       //Substitui "æ" por "a"
         name = name.replace(/¢/g,"c")       //Substitui "¢" por "c"
         name = name.replace(/ø/g,"o")       //Substitui "ø" por "o"
@@ -16,7 +21,7 @@ function fixName(data){
 function fixPrice(data){
     data.forEach(obj => {
         if(typeof obj.price == "string"){
-            var price = parseFloat(obj.price)   //Copia o valor de price e o converte para float
+            let price = parseFloat(obj.price)   //Copia o valor de price e o converte para float
             obj.price = price                   //Registra o valor corrigido
         }
     });
@@ -30,9 +35,20 @@ function fixQuantity(data){
     });
 }
 
+function exportSolutionToJson(data) {
+    let json = JSON.stringify(data)
+    let fs = require('fs')
+    fs.writeFile('files/fixed-database.json',json,'utf8', function(err) {
+        if (err) throw err
+        console.log('Complete')
+    })
+}
+
 const data = readJson();                    //Array com todos os objetos obtidos da leitura do JSON
+if (data == null) {
+    return;
+}
 fixName(data);                              //Chamada da função que corrige os nomes
 fixPrice(data);                             //Chamada da função que corrige os preços
 fixQuantity(data);                          //Chamada da função que corrige as quantidades
-
-console.log(data)
+exportSolutionToJson(data);                 //Chamada da função que exporta um arquivo com a saída
