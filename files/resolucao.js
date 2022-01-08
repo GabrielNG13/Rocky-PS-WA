a92849ea6d0a1cbd401af1b79052ffaef34da0b7
GabrielNG13 <gabrielnunesmghirardelli@gmail.com>
2022-01-08 12:26:11 -0300

    Fix names

--- files/resolucao.js
+++ files/resolucao.js
@@ -2,6 +2,19 @@ function readJson(){
     return require('./broken-database.json')
 }
 
-data = readJson();  //Array com todos os objetos obtidos da leitura do JSON
+function fixNames(data){
+    data.forEach(obj => {
+        var name = obj.name
+        name = name.replace(/æ/g,"a")       //Substituindo "æ" por "a"
+        name = name.replace(/¢/g,"c")       //Substituindo "¢" por "c"
+        name = name.replace(/ø/g,"o")       //Substituindo "ø" por "o"
+        name = name.replace(/ß/g,"b")       //Substituindo "ß" por "b"
+        obj.name = name
+    });
+}
+
+const data = readJson();                    //Array com todos os objetos obtidos da leitura do JSON
+fixNames(data);                             //Chamada da função que corrige os nomes
+
 
 console.log(data)
